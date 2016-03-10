21ec096b83331b20660c97f08d4854d6a7453514
Kurt King <kurtaking@gmail.com>
2016-03-10 00:06:32 -0600

    Updated the intro section and made it fully responsive

--- js/script.js
+++ js/script.js
@@ -9,6 +9,14 @@ $(function() {
     });
 });
 
+// Test JS for closing navbar
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+
+
 $(window).resize(function() {
 	$('#slide-1.slide.story').height($(window).height());
 }).resize();
@@ -220,29 +228,3 @@ jQuery(document).ready(function ($) {
 	});
 });
 
-/***************
-* = Menu hover *
-***************/
-jQuery(document).ready(function ($) {
-	//Cache some variables
-	var menu_item = $('.nav').find('li');
-
-	menu_item.hover(
-		function(e) {
-			var icon = $(this).find('.icon');
-
-			var left_pos = icon.offset().left - $('.nav').offset().left;
-			var el_width = icon.width() + $(this).find('.text').width() + 10;
-
-			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
-				.css('left', left_pos)
-				.css('width', el_width)
-				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-
-			$('.active-menu').after( hover_bar );
-		},
-		function(e) {
-			$('.special-active-menu').remove();
-		}
-	);
-});
